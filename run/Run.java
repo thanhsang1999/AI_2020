d645db5f4e548abce0abf84f9d831ea0e0928e09
thanhsang1999 <68640875+thanhsang1999@users.noreply.github.com>
2020-07-23 17:46:11 +0700

    Update Run.java

--- run/Run.java
+++ run/Run.java
@@ -27,6 +27,7 @@ public class Run extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		// sang nguyen
+		// test 2
 		request.getRequestDispatcher("web/index.jsp").forward(request, response);
 	}
 
