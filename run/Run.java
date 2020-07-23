973882595edbc048f36f70415a8ece74d8925959
= <=>
2020-07-23 19:50:36 +0700

    7:50

--- run/Run.java
+++ run/Run.java
@@ -26,7 +26,6 @@ public class Run extends HttpServlet {
 	private void todo(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
-		// Thanh Sang
 		request.getRequestDispatcher("web/index.jsp").forward(request, response);
 	}
 
