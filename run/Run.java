b9c0e58dcb83c41922fbd7c0cef5be7a9f9b119b
thanhsang1999 <68640875+thanhsang1999@users.noreply.github.com>
2020-07-23 17:40:49 +0700

    Update Run.java

--- run/Run.java
+++ run/Run.java
@@ -26,6 +26,7 @@ public class Run extends HttpServlet {
 	private void todo(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
+		// Nguyen Huy Thanh Sang
 		request.getRequestDispatcher("web/index.jsp").forward(request, response);
 	}
 
